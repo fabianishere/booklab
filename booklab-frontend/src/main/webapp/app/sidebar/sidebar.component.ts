eff862ccfe41ae257aac83f0b6ba6fad810ace1b
Marijn Roelvink <m.a.t.roelvink@student.tudelft.nl>
2018-04-30 12:02:36 +0200

    feat(#11): Create initial frontend prototype in Angular
    This change adds an initial frontend prototype in Angular to the
    repository. The prototype is already integrated with Gradle. However, no
    automated tests are run on the CI server due to dependency on a browser
    for testing.

---
+++ booklab-frontend/src/main/webapp/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
