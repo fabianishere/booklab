c67b2d5e9d060458ede67c49330de4547b6b219c
Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>
2018-05-01 15:19:15 +0200

    Add working gradle dependency for opencv

--- booklab-backend/src/BookDetector.java
+++ booklab-backend/src/BookDetector.java
@@ -1,13 +1,24 @@
-import org.opencv.core.*;
+import org.opencv.core.Mat;
 
 import static org.opencv.imgcodecs.Imgcodecs.imread;
+import static org.opencv.imgcodecs.Imgcodecs.imwrite;
 
 public class BookDetector {
 
-    public void detectBook() {
-//        Loader.load(opencv_java.class);
-        Mat im = imread("test.jpg");
 
+    static {
+        nu.pattern.OpenCV.loadShared();
+        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void detectBook() {
+        Mat im = imread("resources/bookshelf.jpg");
+        imwrite("resources/output.jpg", im);
+        System.out.println("Saved image");
+    }
+
+    public static void main(String[] args) {
+        detectBook();
     }
 
 }
