660e51c0584e5af0cae4471b459c6233abd2b4c4
Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>
2018-05-01 14:22:01 +0200

    Add initial java class for book detector

---
+++ booklab-backend/src/BookDetector.java
@@ -0,0 +1,13 @@
+import org.opencv.core.*;
+
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+
+public class BookDetector {
+
+    public void detectBook() {
+//        Loader.load(opencv_java.class);
+        Mat im = imread("test.jpg");
+
+    }
+
+}
