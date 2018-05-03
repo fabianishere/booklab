5216549375f767344bc8155c5a567802ec602640
Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>
2018-05-03 10:44:46 +0200

    Refactor BookDetector

--- booklab-backend/src/main/java/BookDetector.java
+++ booklab-backend/src/main/java/BookDetector.java
@@ -1,7 +1,5 @@
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.jetbrains.annotations.NotNull;
+import org.opencv.core.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,56 +14,106 @@ import static org.opencv.imgproc.Imgproc.*;
 
 public class BookDetector {
 
-
-    public static final int WINDOW_SIZE = 5;
-
     static {
         nu.pattern.OpenCV.loadShared();
         System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
     }
 
-    public static void detectBook() {
+    public static void detectBooks() {
+        String path = System.getProperty("user.dir");
+        path = path + "/booklab-backend/resources/bookshelf.jpg";
+
+        List<Mat> books = detectBooks(path);
+
+        for (int i = 0; i < books.size(); i++) {
+            imwrite(System.getProperty("user.dir") + "/booklab-backend/resources/books/roi_" + i + ".jpg", books.get(i));
+        }
+    }
+
+    public static List<Mat> detectBooks(String path) {
+        Mat image = imread(path);
+        image = ImgProcessHelper.colorhist_equalize(image);
+        List<Integer> cropLocations = detectBookLocations(image);
+        return cropBooks(image, cropLocations);
+    }
+
+    private static List<Integer> detectBookLocations(Mat image) {
         Mat gray = new Mat();
         Mat dilation = new Mat();
         Mat reduced = new Mat();
         List<Point> coordinates = new ArrayList<>();
 
-        String path = System.getProperty("user.dir");
-        Mat im = imread(path + "/booklab-backend/resources/bookshelf.jpg");
-        im = ImgProcessHelper.colorhist_equalize(im);
-
-        cvtColor(im, gray, COLOR_BGR2GRAY);
+        image = ImgProcessHelper.colorhist_equalize(image);
+        cvtColor(image, gray, COLOR_BGR2GRAY);
         Mat edges = ImgProcessHelper.autoCanny(gray);
         dilate(edges, dilation, new Mat());
         reduce(dilation, reduced, 0, REDUCE_AVG);
         GaussianBlur(reduced, reduced, new Size(), 3);
 
-        for (int i = 0; i < im.cols(); i++) {
+        for (int i = 0; i < image.cols(); i++) {
             coordinates.add(new Point(i, reduced.get(0, i)[0]));
-            line(im, new Point(i, 0), new Point(i, reduced.get(0, i)[0]), new Scalar(255, 255, 0), 1);
         }
 
-        List<Double> yCoor = coordinates.stream().map(a -> a.y).collect(Collectors.toList());
+        List<Integer> localMinima = findLocalMinima(coordinates, 5);
 
-        List<Point> localMinima = new ArrayList<>();
-        for (int i = WINDOW_SIZE; i < coordinates.size() - WINDOW_SIZE; i++) {
-            List<Double> sublist = yCoor.subList(i - WINDOW_SIZE, i + WINDOW_SIZE + 1);
-            if (sublist.indexOf(Collections.min(sublist)) == WINDOW_SIZE) {
-                localMinima.add(coordinates.get(i));
+        List<Integer> cropLocations = new ArrayList<>(localMinima);
+        cropLocations.add(0, 0);
+        cropLocations.add(image.cols());
+
+        return cropLocations;
+    }
+
+    @NotNull
+    private static List<Mat> cropBooks(Mat im, List<Integer> cropLocations) {
+        List<Mat> books = new ArrayList<>();
+        for (int i = 0; i < cropLocations.size() - 1; i++) {
+            Mat book = cropBook(im, cropLocations.get(i), cropLocations.get(i + 1) - cropLocations.get(i), false);
+            books.add(book);
+        }
+        return books;
+    }
+
+    @NotNull
+    private static Mat cropBook(Mat im, int x, int width, boolean strictCrop) {
+        if (!strictCrop) {
+            width = (int) (1.1 * (width + 0.05 * x));
+            x = (int) (0.95 * x);
+        }
+
+        if (x + width > im.cols()) {
+            width = width - (x + width - im.cols());
+        }
+
+        Rect roi = new Rect(x, 0, width, im.rows());
+        return new Mat(im, roi);
+    }
+
+    @NotNull
+    private static List<Integer> findLocalMinima(List<Point> coordinates, int windowSize) {
+        List<Double> yCoordinates = coordinates.stream().map(a -> a.y).collect(Collectors.toList());
+        List<Integer> localMinima = new ArrayList<>();
+        for (int i = windowSize; i < coordinates.size() - windowSize; i++) {
+            List<Double> sublist = yCoordinates.subList(i - windowSize, i + windowSize + 1);
+            if (sublist.indexOf(Collections.min(sublist)) == windowSize) {
+                localMinima.add((int) coordinates.get(i).x);
             }
         }
+        return localMinima;
+    }
 
-        for (int i = 0; i < localMinima.size(); i++) {
-            line(im, new Point(localMinima.get(i).x, 0), new Point(localMinima.get(i).x, im.rows()), new Scalar(0, 255, 0), 2);
+    private static void drawGraphs(Mat reducedImage, Mat originalImage, List<Integer> lineLocations) {
+        for (int i = 0; i < originalImage.cols(); i++) {
+            line(originalImage, new Point(i, 0), new Point(i, reducedImage.get(0, i)[0]), new Scalar(255, 255, 0), 1);
         }
 
-        imwrite(path + "/booklab-backend/resources/lines_eq.jpg", im);
+        for (int i = 0; i < lineLocations.size(); i++) {
+            line(originalImage, new Point(lineLocations.get(i), 0), new Point(lineLocations.get(i), originalImage.rows()), new Scalar(0, 255, 0), 2);
+        }
     }
 
 
-
     public static void main(String[] args) {
-        detectBook();
+        detectBooks();
     }
 
 }
