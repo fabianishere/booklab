0594d3629308f3b1bf7d0d0cd571347a6fa0b5ef
Vera Hoveling <v.t.hoveling@student.tudelft.nl>
2018-05-08 13:44:41 +0200

    Add javadoc to BookDetector.java

--- booklab-backend/src/main/java/BookDetector.java
+++ booklab-backend/src/main/java/BookDetector.java
@@ -1,119 +1,119 @@
-import org.jetbrains.annotations.NotNull;
-import org.opencv.core.*;
+import org.opencv.core.*
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList
+import java.util.Collections
+import java.util.stream.Collectors
 
-import static org.opencv.core.Core.REDUCE_AVG;
-import static org.opencv.core.Core.reduce;
-import static org.opencv.imgcodecs.Imgcodecs.imread;
-import static org.opencv.imgcodecs.Imgcodecs.imwrite;
-import static org.opencv.imgproc.Imgproc.*;
+import org.opencv.core.Core.REDUCE_AVG
+import org.opencv.core.Core.reduce
+import org.opencv.imgcodecs.Imgcodecs.imread
+import org.opencv.imgcodecs.Imgcodecs.imwrite
+import org.opencv.imgproc.Imgproc.*
 
-public class BookDetector {
+object BookDetector {
 
-    static {
-        nu.pattern.OpenCV.loadShared();
-        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+    init {
+        nu.pattern.OpenCV.loadShared()
+        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME)
     }
 
-    public static void detectBooks() {
-        String path = System.getProperty("user.dir");
-        path = path + "/booklab-backend/resources/bookshelf.jpg";
+    fun detectBooks() {
+        var path = System.getProperty("user.dir")
+        path = path + "/booklab-backend/resources/bookshelf.jpg"
 
-        Mat image = imread(path);
-        List<Mat> books = detectBooks(image);
+        val image = imread(path)
+        val books = detectBooks(image)
 
-        for (int i = 0; i < books.size(); i++) {
-            imwrite(System.getProperty("user.dir") + "/booklab-backend/resources/books/roi_" + i + ".jpg", books.get(i));
+        for (i in books.indices) {
+            imwrite(System.getProperty("user.dir") + "/booklab-backend/resources/books/roi_" + i + ".jpg", books[i])
         }
     }
 
-    public static List<Mat> detectBooks(Mat image) {
-        image = ImgProcessHelper.colorhist_equalize(image);
-        List<Integer> cropLocations = detectBookLocations(image);
-        return cropBooks(image, cropLocations, false);
+    fun detectBooks(image: Mat): List<Mat> {
+        var image = image
+        image = ImgProcessHelper.colorhist_equalize(image)
+        val cropLocations = detectBookLocations(image)
+        return cropBooks(image, cropLocations, false)
     }
 
-    private static List<Integer> detectBookLocations(Mat image) {
-        Mat gray = new Mat();
-        Mat dilation = new Mat();
-        Mat reduced = new Mat();
-        List<Point> coordinates = new ArrayList<>();
+    private fun detectBookLocations(image: Mat): List<Int> {
+        var image = image
+        val gray = Mat()
+        val dilation = Mat()
+        val reduced = Mat()
+        val coordinates = ArrayList<Point>()
 
-        image = ImgProcessHelper.colorhist_equalize(image);
-        cvtColor(image, gray, COLOR_BGR2GRAY);
-        Mat edges = ImgProcessHelper.autoCanny(gray);
-        dilate(edges, dilation, new Mat());
-        reduce(dilation, reduced, 0, REDUCE_AVG);
-        GaussianBlur(reduced, reduced, new Size(), 3);
+        image = ImgProcessHelper.colorhist_equalize(image)
+        cvtColor(image, gray, COLOR_BGR2GRAY)
+        val edges = ImgProcessHelper.autoCanny(gray)
+        dilate(edges, dilation, Mat())
+        reduce(dilation, reduced, 0, REDUCE_AVG)
+        GaussianBlur(reduced, reduced, Size(), 3.0)
 
-        for (int i = 0; i < image.cols(); i++) {
-            coordinates.add(new Point(i, reduced.get(0, i)[0]));
+        for (i in 0..image.cols() - 1) {
+            coordinates.add(Point(i.toDouble(), reduced.get(0, i)[0]))
         }
 
-        List<Integer> localMinima = findLocalMinima(coordinates, 5);
+        val localMinima = findLocalMinima(coordinates, 5)
 
-        List<Integer> cropLocations = new ArrayList<>(localMinima);
-        cropLocations.add(0, 0);
-        cropLocations.add(image.cols());
+        val cropLocations = ArrayList(localMinima)
+        cropLocations.add(0, 0)
+        cropLocations.add(image.cols())
 
-        return cropLocations;
+        return cropLocations
     }
 
-    @NotNull
-    private static List<Mat> cropBooks(Mat im, List<Integer> cropLocations, boolean strictCrop) {
-        List<Mat> books = new ArrayList<>();
-        for (int i = 0; i < cropLocations.size() - 1; i++) {
-            Mat book = cropBook(im, cropLocations.get(i), cropLocations.get(i + 1) - cropLocations.get(i), strictCrop);
-            books.add(book);
+    private fun cropBooks(im: Mat, cropLocations: List<Int>, strictCrop: Boolean): List<Mat> {
+        val books = ArrayList<Mat>()
+        for (i in 0..cropLocations.size - 1 - 1) {
+            val book = cropBook(im, cropLocations[i], cropLocations[i + 1] - cropLocations[i], strictCrop)
+            books.add(book)
         }
-        return books;
+        return books
     }
 
-    @NotNull
-    private static Mat cropBook(Mat im, int x, int width, boolean strictCrop) {
+    private fun cropBook(im: Mat, x: Int, width: Int, strictCrop: Boolean): Mat {
+        var x = x
+        var width = width
         if (!strictCrop) {
-            width = (int) (1.1 * (width + 0.05 * x));
-            x = (int) (0.95 * x);
+            width = (1.1 * (width + 0.05 * x)).toInt()
+            x = (0.95 * x).toInt()
         }
 
         if (x + width > im.cols()) {
-            width = width - (x + width - im.cols());
+            width = width - (x + width - im.cols())
         }
 
-        Rect roi = new Rect(x, 0, width, im.rows());
-        return new Mat(im, roi);
+        val roi = Rect(x, 0, width, im.rows())
+        return Mat(im, roi)
     }
 
-    @NotNull
-    private static List<Integer> findLocalMinima(List<Point> coordinates, int windowSize) {
-        List<Double> yCoordinates = coordinates.stream().map(a -> a.y).collect(Collectors.toList());
-        List<Integer> localMinima = new ArrayList<>();
-        for (int i = windowSize; i < coordinates.size() - windowSize; i++) {
-            List<Double> sublist = yCoordinates.subList(i - windowSize, i + windowSize + 1);
+    private fun findLocalMinima(coordinates: List<Point>, windowSize: Int): List<Int> {
+        val yCoordinates = coordinates.stream().map { a -> a.y }.collect<List<Double>, Any>(Collectors.toList())
+        val localMinima = ArrayList<Int>()
+        for (i in windowSize..coordinates.size - windowSize - 1) {
+            val sublist = yCoordinates.subList(i - windowSize, i + windowSize + 1)
             if (sublist.indexOf(Collections.min(sublist)) == windowSize) {
-                localMinima.add((int) coordinates.get(i).x);
+                localMinima.add(coordinates[i].x.toInt())
             }
         }
-        return localMinima;
+        return localMinima
     }
 
-    private static void drawGraphs(Mat reducedImage, Mat originalImage, List<Integer> lineLocations) {
-        for (int i = 0; i < originalImage.cols(); i++) {
-            line(originalImage, new Point(i, 0), new Point(i, reducedImage.get(0, i)[0]), new Scalar(255, 255, 0), 1);
+    private fun drawGraphs(reducedImage: Mat, originalImage: Mat, lineLocations: List<Int>) {
+        for (i in 0..originalImage.cols() - 1) {
+            line(originalImage, Point(i.toDouble(), 0.0), Point(i.toDouble(), reducedImage.get(0, i)[0]), Scalar(255.0, 255.0, 0.0), 1)
         }
 
-        for (int i = 0; i < lineLocations.size(); i++) {
-            line(originalImage, new Point(lineLocations.get(i), 0), new Point(lineLocations.get(i), originalImage.rows()), new Scalar(0, 255, 0), 2);
+        for (i in lineLocations.indices) {
+            line(originalImage, Point(lineLocations[i].toDouble(), 0.0), Point(lineLocations[i].toDouble(), originalImage.rows().toDouble()), Scalar(0.0, 255.0, 0.0), 2)
         }
     }
 
 
-    public static void main(String[] args) {
-        detectBooks();
+    @JvmStatic
+    fun main(args: Array<String>) {
+        detectBooks()
     }
 
 }
