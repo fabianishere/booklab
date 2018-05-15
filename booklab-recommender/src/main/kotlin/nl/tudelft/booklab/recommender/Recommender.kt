9a33e9c038accc1e709ac023f58b8a764909e3e5
wollemat <f.c.slothouber@student.tudelft.nl>
2018-05-15 10:48:42 +0200

    feat(#23): Add initial recommendations based on NUR code

--- booklab-recommender/src/main/kotlin/nl/tudelft/booklab/recommender/Recommender.kt
+++ booklab-recommender/src/main/kotlin/nl/tudelft/booklab/recommender/Recommender.kt
@@ -18,8 +18,35 @@ package nl.tudelft.booklab.recommender
 
 import nl.tudelft.booklab.catalogue.sru.Book
 
+/**
+ * A singleton object that is able to recommend a selection of books based
+ * on the nur (Nederlandstalige Uniforme Rubrieksindeling) code that is stored
+ * in the database
+ *
+ * @author Christian Slothouber (f.c.slothouber@student.tudelft.nl)
+ */
 object Recommender {
+
+    /**
+     * The recommendation function
+     *
+     * @param collection The list of [Book]s that is used to determine the preference
+     * @param candidates The list of [Book]s that can be recommended
+     * @return a list of [Book]s that contain the recommended books
+     */
     fun recommend(collection: List<Book>, candidates: List<Book>): List<Book> {
+        val tags = collection
+            .map { it.nur }
+            .groupBy { it }
+            .entries.sortedByDescending { it.value.size }
+            .map { it.key }
+            .filter { it != -1 }
+        for (i in 0 until tags.size) {
+            val selection = candidates.filter { it.nur == tags[i] }
+            if (selection.isNotEmpty()) {
+                return selection
+            }
+        }
         return emptyList()
     }
 }
