8639294a0419f1d93432153f7018f90828438c93
Vera Hoveling <v.t.hoveling@student.tudelft.nl>
2018-05-16 09:40:05 +0200

    Test BookDetector class

---
+++ booklab-backend/src/test/java/nl/tudelft/booklab/backend/ocr/BookDetectorTest.java
@@ -0,0 +1,24 @@
+package nl.tudelft.booklab.backend.ocr;
+
+import nl.tudelft.nlbooklab.backend.ocr.BookDetector;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opencv.core.Mat;
+import java.util.List;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+public class BookDetectorTest {
+
+    static {
+        nu.pattern.OpenCV.loadShared();
+        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Test
+    public void bookDetectorTest(){
+        String path = System.getProperty("user.dir") + "/src/test/java/nl/tudelft/booklab/backend/ocr/testbookshelf.jpg";
+        Mat img = imread(path);
+        List<Mat> books = BookDetector.detectBooks(img);
+
+        Assert.assertTrue(books.size() > 0);
+    }
+}
