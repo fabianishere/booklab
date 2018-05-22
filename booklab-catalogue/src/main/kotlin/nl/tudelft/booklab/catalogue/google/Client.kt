baebbf614fd5972d67d7c884498294d263631a57
wollemat <f.c.slothouber@student.tudelft.nl>
2018-05-22 11:25:09 +0200

    feat(#42): Add google client library and api key

---
+++ booklab-catalogue/src/main/kotlin/nl/tudelft/booklab/catalogue/google/Client.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 The BookLab Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.tudelft.booklab.catalogue.google
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.books.Books
+import com.google.api.services.books.BooksRequestInitializer
+import com.google.api.services.books.model.Volumes
+
+
+object GoogleClient {
+    fun query(query: String): Volumes {
+        val key = ""
+        val books = Books.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), null)
+            .setApplicationName("booklab")
+            .setGoogleClientRequestInitializer(BooksRequestInitializer(key))
+            .build()
+        val volumesList = books.volumes().list(query)
+        return volumesList.execute()
+    }
+}
+
