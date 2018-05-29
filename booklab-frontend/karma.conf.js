5a44e0b257bc98938181e76394ae146dfc644ee7
Fabian Mastenbroek <mail.fabianm@gmail.com>
2018-05-29 22:51:21 +0200

    chore(#62): Add docs as build artifacts
    This change will add generated documentation, test reports and build
    files to the generated artifacts when run on the master branch.

--- booklab-frontend/karma.conf.js
+++ booklab-frontend/karma.conf.js
@@ -9,7 +9,7 @@ try {
 
 module.exports = function (config) {
     config.set({
-        basePath: '',
+        basePath:  __dirname,
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
@@ -17,20 +17,25 @@ module.exports = function (config) {
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
-            require('karma-verbose-reporter')
+            require('karma-verbose-reporter'),
+            require('karma-html-reporter')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, 'coverage'),
+            dir: require('path').join(__dirname, 'build/reports/istanbul'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
+        },
+        htmlReporter: {
+            outputDir: 'build/reports/karma',
+            urlFriendlyName: true
         },
         angularCli: {
             environment: 'dev'
         },
-        reporters: ['verbose', 'kjhtml'],
+        reporters: ['verbose', 'html', 'kjhtml'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
