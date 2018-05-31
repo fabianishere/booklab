e4c80675645f8c6b8f3d07384a47c9ea10b62595
Vera Hoveling <v.t.hoveling@student.tudelft.nl>
2018-05-31 13:30:12 +0200

    Adjust interface and navigation slightly

--- booklab-frontend/src/app/app.routing.ts
+++ booklab-frontend/src/app/app.routing.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {ImageUploadComponent} from "./components/image-upload/image-upload.component";
 import {BookshelfComponent} from "./components/bookshelf/bookshelf.component";
 import {AboutComponent} from "./components/about/about.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
     {
@@ -16,6 +17,11 @@ const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+    }
+    ,
+    {
+        path: 'home',
+        component: HomeComponent
     }
 ];
 
