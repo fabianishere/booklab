fa52b26e5fc6ed90f570d607a8ce7f20ba6f1ec7
wollemat <f.c.slothouber@student.tudelft.nl>
2018-06-04 12:41:32 +0200

    feat(#78): Create Android module setup
    This change adds a Gradle module for the Android application we are
    building as part of this project.

---
+++ booklab-frontend-android/src/main/kotlin/nl/tudelft/booklab/frontend/android/MainActivity.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 The BookLab Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.tudelft.booklab.frontend.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.button
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MainActivityView().setContentView(this)
+    }
+}
+
+class MainActivityView : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>) = ui.apply {
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { ctx.toast("Hello, ${name.text}!") }
+            }
+        }
+    }.view
+}
+
