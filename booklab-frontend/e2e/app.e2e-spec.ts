73e1e54b7163fe45b89a9a2093e514ef73a3f832
Marijn Roelvink <m.a.t.roelvink@student.tudelft.nl>
2018-06-04 11:13:20 +0200

    Test(#65): Add first succesful e2e test

--- booklab-frontend/e2e/app.e2e-spec.ts
+++ booklab-frontend/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('book-lab App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getHeader()).toBeTruthy();
   });
 });
