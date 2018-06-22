3a7f82cf6ec6e0b541ccc6d7ac25a403e70d9669
Marijn Roelvink <m.a.t.roelvink@student.tudelft.nl>
2018-06-23 00:29:02 +0200

    Fix lint errors

--- booklab-backend/src/main/kotlin/nl/tudelft/booklab/backend/Application.kt
+++ booklab-backend/src/main/kotlin/nl/tudelft/booklab/backend/Application.kt
@@ -24,7 +24,11 @@ import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.auth.oauth2.oauth
-import io.ktor.features.*
+import io.ktor.features.CORS
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.DataConversion
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.jackson.jackson
